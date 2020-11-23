c576b399c3faedd8708c9c7b678065fc492bad3b
theabbie <abhishek7gg7@gmail.com>
2020-11-23 20:14:49 +0530

    Fixed Errors (No Backward Compatibility)

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 var axios = require("axios");
 var unshort = require("unshorten.it");
+var crypto = require('crypto');
+
+var uuid = 'v6-1605760737061-17c8efdf-00ea-42bf-8a09-2dc50b9c417a';
+var appid = 'com.ninegag.android.app';
+var ts = Date.now().toString();
+//var token = crypto.createHash('sha1').update(ts).digest('hex');
+var token = 'Y29tLm5pbmVnYWcuYW5kcm9pZC5hcHAqMmt1ZTd0czBj';
+var sig = crypto.createHash('sha1').update(`*${ts}_._${appid}._.${uuid}9GAG`).digest('hex');
 
 module.exports = async function(gag_id) {
   if (!gag_id) {
     var gag = await unshort("https://9gag.com/shuffle");
     var gag_id = gag.split("/").reverse()[0];
   }
+  
   var headers = {
     'Host': 'api.9gag.com',
     'user-agent': '9GAG/6920200 (Xiaomi Redmi Note 7 Pro;Android 10)',
-    '9gag-9gag_token': 'Y29tLm5pbmVnYWcuYW5kcm9pZC5hcHAqNzVqaXdmNGhw',
-    '9gag-timestamp': '1605762948028',
-    '9gag-app_id': 'com.ninegag.android.app',
-    '9gag-device_uuid': 'v6-1605760737061-17c8efdf-00ea-42bf-8a09-2dc50b9c417a',
-    '9gag-request-signature': '4721536ef9d0f73d237356f370e87e24e9019799',
-    'x-package-id': 'com.ninegag.android.app',
+    '9gag-9gag_token': token,
+    '9gag-timestamp': ts,
+    '9gag-app_id': appid,
+    '9gag-device_uuid': uuid,
+    '9gag-request-signature': sig,
+    'x-package-id': appid,
     'x-package-version': '6920200',
-    'x-device-uuid': 'v6-1605760737061-17c8efdf-00ea-42bf-8a09-2dc50b9c417a',
+    'x-device-uuid': uuid,
     'accept-encoding': 'gzip'
   };
 
